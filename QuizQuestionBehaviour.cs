9380efb6f86f55d40b89c423bea5a18f833dd327
syikili <nurasyikinzulkafli@gmail.com>
2017-10-16 04:07:37 +0800

    Add files via upload

--- QuizQuestionBehaviour.cs
+++ QuizQuestionBehaviour.cs
@@ -17,8 +17,14 @@ public class QuizQuestionBehaviour : MonoBehaviour {
     public Sprite OffSprite, OnSprite;
     private DatabaseReference _databaseReference;
     private string path = "Questions/Topic/Solat/se2";
+    private string answerPath = "/Student_Users/1/Stud_Answer/Topic/Solat/se1/q1/";
+    private string answer,q;
+    private bool statusJawapan, buttonClicked;
+    private int i = 0;
+    public string questionNumber = "q" +1;
 
-
+    /*-------------------------------------------------------------------------------------------------------------*/
+    //behaviour of interface
     public void ChangeImageButton()
     {
         if (answerBtn.image.sprite == OnSprite)
@@ -26,13 +32,17 @@ public class QuizQuestionBehaviour : MonoBehaviour {
             answerBtn.image.sprite = OffSprite;
             buttonText.text = "Submit";
             newQuestion = false;
+            Debug.Log(newQuestion);
         }
 
-        else
+        else 
         {
             answerBtn.image.sprite = OnSprite;
             buttonText.text = "Next";
             newQuestion = true;
+            Debug.Log(newQuestion);
+            //Next_Question();
+
         }
     }
 
@@ -43,7 +53,7 @@ public class QuizQuestionBehaviour : MonoBehaviour {
         wrongIcon.enabled = false;
 
     }
-
+    
     public void Answer_CorrectOutput()
     {
         answerStatus.text = "Correct !";
@@ -63,13 +73,46 @@ public class QuizQuestionBehaviour : MonoBehaviour {
 
     public void Button_OnClick()
     {
-        Answer_WrongOutput();
+        buttonClicked = true;
+        Answer_WrongOutput();   
         //Changes the button's Normal color to the new color.
         //answerBtn.image.color = Color.blue;
         ChangeImageButton();
-        Debug.Log(newQuestion);
+        Next_Question();
+        //Debug.Log(newQuestion);
+
+        //grab student answer and answer status
+        /*answer = answerInput.text;
+        q = "q1";
+        Debug.Log(answer);
+        statusJawapan = false;
+        Debug.Log(statusJawapan);*/
+
+        //Write_StudentAnswer(  "q1" , answerInput.text, statusJawapan);
 
 
+    }
+
+    public string Next_Question()
+    {
+       
+        
+        if (newQuestion == true && buttonClicked == true)
+        {
+            //for(int i=0; i<11; i++)
+            {
+                i++;
+                questionNumber = "q"+ i;
+                Debug.Log(questionNumber);
+                buttonClicked = false;
+                newQuestion = false;
+            }
+
+            
+
+        }
+
+        return questionNumber;
     }
 
 	// Use this for initialization
@@ -80,7 +123,7 @@ public class QuizQuestionBehaviour : MonoBehaviour {
         // Get the root reference location of the database.
         _databaseReference = FirebaseDatabase.DefaultInstance.RootReference;
 
-        ReadQuestion("q1");
+        ReadQuestion(questionNumber);
 
         //hide element
         Hide();
@@ -92,13 +135,16 @@ public class QuizQuestionBehaviour : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+        //Next_Question();
+        ReadQuestion(questionNumber);
 	}
 
-    //Reading from Firebase Database
+    /*-------------------------------------------------------------------------------------------------------------*/
+
+    //Reading questions from Firebase Database
     void ReadQuestion(string qid)
     {
-        FirebaseDatabase.DefaultInstance.GetReference(path).Child("q1").GetValueAsync().ContinueWith(task =>
+        FirebaseDatabase.DefaultInstance.GetReference(path).Child(questionNumber).GetValueAsync().ContinueWith(task =>
         {
             if (task.IsFaulted)
             {
@@ -108,21 +154,69 @@ public class QuizQuestionBehaviour : MonoBehaviour {
             {
                 DataSnapshot snapshot = task.Result;
                 string json1 = snapshot.Child("question_no").GetRawJsonValue();
-                string json2 = snapshot.Child("question_text").GetRawJsonValue();
+                string json2 = snapshot.Child("question_text").GetRawJsonValue().Trim("\"".ToCharArray());
                 string json3 = snapshot.Child("question_point").GetRawJsonValue();
                 string json4 = snapshot.Child("correct_answer").GetRawJsonValue();
-                string json5 = snapshot.Child("username").GetRawJsonValue();
+
 
 
                 // var index = json1.Length;
 
                 questionText.text = json2.Trim("\"".ToCharArray());
-                userpoints.text = json3.Trim("\"".ToCharArray());
+                userpoints.text = json3 + " points";
                 
 
                 Debug.Log("Question " + json1 + ": " + json2);
             }
         }
             );
+    }
+
+    /*-------------------------------------------------------------------------------------------------------------*/
+
+    //Writing student answers into Firebase Database
+    //STILL CANNOT !!
+
+    public class NewAnswer{
+
+        private string stud_answer, question;
+        private bool isCorrect;
+
+        public NewAnswer()
+        {
+        }
+
+        public NewAnswer( string stud_answer, bool isCorrect)
+        {
+            this.stud_answer = stud_answer;
+            this.isCorrect = isCorrect;
+            //this.question = question;
+        }
+      
+
+    }
+
+    void Write_StudentAnswer( string question, string stud_answer, bool isCorrect) {
+
+        var studAns = new NewAnswer( stud_answer, isCorrect);
+        string json = JsonUtility.ToJson(studAns);
+
+        //write to json format
+        _databaseReference
+            
+            .Child(answerPath)
+            .Child(question)
+            .SetRawJsonValueAsync(json);
+
+       
+        Debug.Log("Answer Saved !");
+
+
+        // When writing the value directly
+        //      _databaseReference
+        //          .Child ("users")
+        //          .Child (userId.ToString ())
+        //          .Child (questId.ToString ())
+        //          .SetValueAsync (isCompleted);
     }
 }
